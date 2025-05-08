9dea09e4de0ffbf388d6ed44dcb67fe3b5fcc84f
Anjali kumari <anjalisinghbhanushali@gmail.com>
2025-05-08 23:12:06 +0530

    update code commit

--- todo1.js
+++ todo1.js
@@ -1,51 +1,38 @@
-
 let todoList = [
-    {
-      item:'buy milk',
-      dueDate:'4/10/11'
-    },
-    {
-        item:'go to college',dueDate:'5/10/2025'
-    }
-    ];
-    displayItems();
-    
-    function addTodo(){
-        let inputElement = document.querySelector('#task');
-    
-        let dateElement = document.querySelector('#todo-date');
-    
-    
-        let todoItem = inputElement.value;
-    
-        let todoDate = dateElement.value;
-    
-        todoList.push(
-        { 
-          item: todoItem,
-          dueDate:todoDate 
-        }
-       );
-    
-        inputElement.value = '';
-        dateElement.value = '';
-    
-        displayItems();
-    }
-    
-    function displayItems(){  
-       let containerElement = document.querySelector('.todo-container');
-       let newHtml = '';
-    
-       for(let i=0;i<todoList.length;i++)
-      {
-        let {item, dueDate} = todoList[i];
-        newHtml += `
-       <span>${item}</span>
-        <span>${dueDate}</span>
-       <button class='btn-delete' onclick="todoList.splice(${i}, 1);
-       displayItems();">Delete</button>
-       `;
-      }
-       containerElement.innerHTML = newHtml;
-    }
+  {
+    item: 'Buy Milk',
+    dueDate: '4/10/2023'
+  },
+  {
+    item: 'Go to College',
+    dueDate: '4/10/2023'
+  }
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+  for (let i = 0; i < todoList.length; i++) {
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+      <span>${item}</span>
+      <span>${dueDate}</span>
+      <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+      displayItems();">Delete</button>
+    `;
+  }
+  containerElement.innerHTML = newHtml;
+}
